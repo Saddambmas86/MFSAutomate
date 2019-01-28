21620420661591b1d361a1903169c111c7f74919
Husain <shusain@ceb.com>
2019-01-29 00:47:48 +0530

    Add one feature

--- src/package1/ClassQa.java
+++ src/package1/ClassQa.java
@@ -14,32 +14,11 @@ import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
 public class ClassQa extends ExtentReportQA{
-
 	
-/*public static void show() throws IOException{
-System.out.println("Amamzon and Flash");
-/*ExtentHtmlReporter htmlReport=new ExtentHtmlReporter("./ExtentReport/Report.html");
-ExtentReports Extent=new ExtentReports();
-Extent.attachReporter(htmlReport);
-test=Extent.createTest("Show Failed");
-test.log(Status.INFO, "Login to Google");
-test.log(Status.FAIL, "Failed Test");
-test.log(Status.FAIL, "Failed Test3434343");
-test.log(Status.PASS, "Passed Test");
-test.log(Status.SKIP, "Skipped Test");
-Extent.flush();
-System.out.println("Verified");
-ExtentTest test1=Extent.createTest("Show Failed1");
-test1.log(Status.INFO, "Login to amazon");
-test1.log(Status.FAIL, "2nd Status");
-test1.fail("Failed due to", MediaEntityBuilder.createScreenCaptureFromPath("./screenshots/A.png").build());
-Extent.flush();
-ExtentTest test2=Extent.createTest("Show Failed1");
-test2.log(Status.INFO, "Login to amazon2");
-test2.log(Status.FAIL, "3rd Status");
-Extent.flush();
+@Test	
+public void show(){
+	System.out.println("QA Test");
 }
-*/
 	
 @Test
 public void Initiate(){
